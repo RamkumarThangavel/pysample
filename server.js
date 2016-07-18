757fe16aa73a69ce67f9b0bbc6c93884a94a58f7
Ramani S <Ramani S>
2016-07-18 22:02:46 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+var server = http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+});
+
+var port = Number (process.env.PORT || 8081);
+server.listen(port);
